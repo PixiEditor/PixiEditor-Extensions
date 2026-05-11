12fa769ab70551b3c7527bd90084f3f87c6473bd
Krzysztof Krysiński <flubbet@gmail.com>
2026-05-11 12:07:26 +0200

    Added spray tool

---
+++ PixiEditor.SprayTool/src/lib.rs
@@ -0,0 +1,30 @@
+  use pixieditor_sdk::*;
+
+  #[unsafe(no_mangle)]
+  pub extern "C" fn initialize() {
+      let spray = CustomToolConfig {
+          name: "SPRAY".to_string(),
+          icon: "/Icons/spray-can.svg".to_string(),
+          tool_tip: "SPRAY_TOOLTIP".to_string(),
+          default_shortcut: Some(Shortcut { key: 0, modifiers: 0 }),
+          supports_secondary_action_on_right_click: false,
+          actions_display_configs: vec![
+              ActionDisplayConfig {
+                  text: "SPRAY_ACTION_DISPLAY".to_string(),
+                  modifiers: 0
+              }
+          ],
+          common_tool_type: "".to_string()
+      };
+
+      let mut buf_spray = Vec::new();
+      spray.encode(&mut buf_spray).unwrap();
+
+      register_brush(
+          load_resource("/Spray.pixi").as_slice(),
+          &buf_spray,
+      );
+
+      add_to_toolset("SPRAY", "PixiEditor:PAINT_TOOLSET", 8);
+      add_to_toolset("SPRAY", "PixiEditor:PIXEL_ART_TOOLSET", 8);
+  }
